c6d65e57c24e131410fd7c2ae4f5235939c1c44b
ruthwekesa <wekesaruth569@gmail.com>
2022-08-05 13:47:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    const thankYouCards = [];
+    for (let i = 0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
